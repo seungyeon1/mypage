09d20517d1b0d3a0c192a7f58f8fb6a45557f052
seungyeon1 <953dltmddus@gmail.com>
2016-08-30 15:13:31 +0900

    0830-01

--- chamisulsoju/js/main.js
+++ chamisulsoju/js/main.js
@@ -1,11 +1,45 @@
 $(function(){
-      $('.visual_control ul li a').click(function(){
-          idx=$(this).parent().index();
-      })
+    idxs = 0;
+    $('.visual_control > ul > li > a').click(function(){
+
+        idxs = $(this).parent().index();
+        var onidx = $('.main_rolling > ul > li.on').index();
+
+        console.log(onidx)
+
+        if(idxs > onidx){
+            $('.main_rolling > ul> li').eq(idxs).addClass('on').children('img').css({
+                top:100 +'%',
+                'display':'block'
+            }).animate({
+                top:0
+            })
+            $('.main_rolling > ul> li').eq(onidx).removeClass('on').children('img').animate({
+                top: - 100 +'%',
+            })
+        }else if(onidx > idxs){
+            $('.main_rolling > ul> li').eq(idxs).addClass('on').children('img').css({
+                top:- 100 +'%',
+                'display':'block'
+            }).animate({
+                top:0
+            })
+            $('.main_rolling > ul> li').eq(onidx).removeClass('on').children('img').animate({
+                top:100 +'%',
+            })
+        }
         return false;
+    })
 
 
-})//big function end//
+
+
+})
+//big function end//
+
+
+// -----------------first video popup---------------
+
 // $(window).load(function(){
 //   $('.main_dimmed').fadeIn();
 //
